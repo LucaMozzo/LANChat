991b172ca8dc0f828dc50d2d645798a2ccfe16f3
lucamozzo <lucamozzo21173@gmail.com>
2016-02-27 00:01:01 +0000

    Added UserList control

---
+++ LANChat/LANChat/Components/UsersList.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LANChat.Components
+{
+    public partial class UsersList : UserControl
+    {
+        public List<User> users { get; }
+
+        public UsersList()
+        {
+            InitializeComponent();
+            users = new List<User>();
+            //test only
+            users.Add(new User("Test1"));
+            users.Add(new User("Test2"));
+            userList.ItemsSource = users;
+        }
+
+        /// <summary>
+        /// Add a user to the list of connected users
+        /// </summary>
+        /// <param name="user">User to be included in the list</param>
+        public void UserConnected(User user)
+        {
+            users.Add(user);
+        }
+
+        /// <summary>
+        /// Remove a user from disconnected users
+        /// </summary>
+        /// <param name="user">User to be removed</param>
+        public void UserDisconnected(User user)
+        {
+            users.Remove(user);
+        }
+    }
+}
