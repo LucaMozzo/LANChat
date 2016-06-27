1b58fe5efea8731803c2c39db429fcceea0c074c
Luca <lucamozzo21173@gmail.com>
2016-06-28 00:00:16 +0200

    Revert "Project files reloaded"
    This reverts commit d92cde72099e6e5150fa4f19b9e7e748c4027094.

---
+++ LANChat/LANChat/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+
+namespace LANChat
+{
+    /// <summary>
+    /// A user connected to the server
+    /// </summary>
+    public class User
+    {
+        public String UserName { get; private set; }
+        public IPAddress IP { get; private set; }
+
+        /// <summary>
+        /// Constructor to create a new user sets the given name and retrieves local IP address
+        /// </summary>
+        /// <param name="userName">The public username</param>
+        public User(String userName)
+        {
+            UserName = userName;
+            IP = getIPAddress();
+        }
+
+        /// <summary>
+        /// Retrieves the local IPv6 address
+        /// </summary>
+        /// <returns></returns>
+        private IPAddress getIPAddress()
+        {
+            String strHostName = Dns.GetHostName();
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+            return addr[0]; //IPv6
+        }
+
+        public override string ToString()
+        {
+            return UserName + ": " + IP.ToString();
+        }
+    }
+}
