eaa83fb7099a8a0fd2f2241354a0c633f82c9cee
Luca <lucamozzo21173@gmail.com>
2016-06-28 00:02:21 +0200

    Revert "Added logic for authentication validation"
    This reverts commit 1903e7129fb024eccd7e35c939f945a9014c105c.

--- LANChat/LANChat/AuthenticationWindow.xaml.cs
+++ LANChat/LANChat/AuthenticationWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,11 +18,6 @@ namespace LANChat
     /// </summary>
     public partial class AuthenticationWindow : Window
     {
-        private string pattern = "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})";
-
-        /// <summary>
-        /// Default constructor
-        /// </summary>
         public AuthenticationWindow()
         {
             InitializeComponent();
@@ -37,38 +31,6 @@ namespace LANChat
         private void remoteServer_Unchecked(object sender, RoutedEventArgs e)
         {
             expander.IsEnabled = false;
-        }
-
-        private void loginBtn_Click(object sender, RoutedEventArgs e)
-        {
-            //password may be empty but username can't
-            if (usernameTxt.Text != String.Empty)
-            {
-                if (remoteServer.IsChecked.Value)
-                    if (Regex.Match(addressTxt.Text, pattern).Success && isNumber(portTxt.Text))
-                    {
-                        //TODO: authenticate
-
-                        //shows the chat window
-                        MainWindow main = new MainWindow();
-                        main.Show();
-                        Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("You have not entered a valid address/port combination to connect to the server", "Validation error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
-            }
-            else
-                MessageBox.Show("The username cannot be empty", "Empty username", MessageBoxButton.OK, MessageBoxImage.Warning);
-        }
-
-        private static bool isNumber(string text)
-        {
-            foreach (char letter in text.ToCharArray())
-                if (!((int)letter >= 48 && (int)letter <= 57))
-                    return false;
-            return true;
         }
     }
 }
