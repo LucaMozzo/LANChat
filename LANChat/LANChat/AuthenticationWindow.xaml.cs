1c92e0e786fa38a2cdf49289fd33fb8ec4812c77
Luca <lucamozzo21173@gmail.com>
2016-06-27 22:18:45 +0200

    Added Authentication window GUI

---
+++ LANChat/LANChat/AuthenticationWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LANChat
+{
+    /// <summary>
+    /// Interaction logic for AuthenticationWindow.xaml
+    /// </summary>
+    public partial class AuthenticationWindow : Window
+    {
+        public AuthenticationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void remoteServer_Checked(object sender, RoutedEventArgs e)
+        {
+            expander.IsEnabled = true;
+        }
+
+        private void remoteServer_Unchecked(object sender, RoutedEventArgs e)
+        {
+            expander.IsEnabled = false;
+        }
+    }
+}
