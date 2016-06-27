1ea0981352c928e8368a835a86f18f0e62626cc2
Luca <lucamozzo21173@gmail.com>
2016-06-28 00:33:48 +0200

    Commit everything

---
+++ LANChat/LANChat Server/Database.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Data;
+
+namespace LANChat_Server
+{
+    /// <summary>
+    /// Utility for communicating with SQLite database
+    /// </summary>
+    class Database
+    {
+        /// <summary>
+        /// Executes a query and returns the result
+        /// </summary>
+        /// <param name="query">The query</param>
+        /// <returns>The result</returns>
+        public static DataTable ExecuteQuery(String query)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SQLiteConnection connection = new SQLiteConnection("Data Source=data.db");
+                connection.Open();
+                SQLiteCommand command = new SQLiteCommand(connection);
+                command.CommandText = query;
+                SQLiteDataReader reader = command.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                connection.Close();
+            }
+            catch(Exception ex) { }
+
+            return dt;
+        }
+
+        /// <summary>
+        /// Runs SQL that is not a query
+        /// </summary>
+        /// <param name="sql">The command</param>
+        /// <returns>Number of affected rows</returns>
+        public static int ExecuteNonQuery(String sql)
+        {
+            SQLiteConnection connection = new SQLiteConnection("Data Source=data.db");
+            connection.Open();
+            SQLiteCommand command = new SQLiteCommand(connection);
+            command.CommandText = sql;
+            int rowsUpdated = command.ExecuteNonQuery();
+            connection.Close();
+
+            return rowsUpdated;
+        }
+    }
+}
---
+++ LANChat/LANChat Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace LANChat_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            Console.Title = fileVersionInfo.ProductName;
+            Console.WriteLine(fileVersionInfo.ProductName + " " + fileVersionInfo.ProductVersion + "\nCopyright (c) 2016 Luca Mozzo.\n");
+
+            Console.WriteLine("Server is starting...");
+
+            Console.ReadKey();
+        }
+    }
+}
