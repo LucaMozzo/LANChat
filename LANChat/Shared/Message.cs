73b7d77f535266570d84892e626bcfdbc7be02ca
Luca <lucamozzo21173@gmail.com>
2016-07-04 13:53:10 +0200

    Added shared project with command and message class

---
+++ LANChat/Shared/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+
+namespace Shared
+{
+    class Message<T>
+    {
+        public Command command { get; private set; }
+        public IPAddress sender { get; private set; }
+        public IPAddress receiver { get; private set; }
+        public T content { get; private set; }
+    }
+}
