9dde73195f0c3efebff79f6ce7bbf864865a886f
Luca <lucamozzo21173@gmail.com>
2016-07-05 09:09:13 +0200

    Allowed serialization of the message

--- LANChat/Shared/Message.cs
+++ LANChat/Shared/Message.cs
@@ -5,11 +5,12 @@ using System.Net;
 
 namespace Shared
 {
-    class Message<T>
+    [Serializable]
+    public class Message : EventArgs
     {
-        public Command command { get; private set; }
-        public IPAddress sender { get; private set; }
-        public IPAddress receiver { get; private set; }
-        public T content { get; private set; }
+        public Command command { get; set; }
+        public IPAddress sender { get; set; }
+        public IPAddress receiver { get; set; }
+        public object content { get; set; }
     }
 }
