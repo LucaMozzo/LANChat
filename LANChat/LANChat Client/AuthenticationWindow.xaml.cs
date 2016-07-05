dda063ff498c6dd86537cbabcfa395012043647f
Luca <lucamozzo21173@gmail.com>
2016-07-05 11:22:00 +0200

    Improved client with authentication working properly

--- LANChat/LANChat Client/AuthenticationWindow.xaml.cs
+++ LANChat/LANChat Client/AuthenticationWindow.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LANChat_Core;
+using System.Net;
+using Shared;
+using System.Net.Sockets;
 
 namespace LANChat_Client
 {
@@ -25,12 +29,65 @@ namespace LANChat_Client
 
         private void remoteServer_Checked(object sender, RoutedEventArgs e)
         {
-            expander.IsEnabled = true;
+            addressTxt.IsEnabled = true;
         }
 
         private void remoteServer_Unchecked(object sender, RoutedEventArgs e)
         {
-            expander.IsEnabled = false;
+            addressTxt.Text = "127.0.0.1";
+            addressTxt.IsEnabled = false;
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(userTxt.Text == "" || portTxt.Text == "" || addressTxt.Text == "")
+            {
+                MessageBox.Show("Check the username, the port and the server address - they can't be empty", "Data entered incorrectly", 
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+
+            }
+
+            try
+            {
+                IPAddress addr = IPAddress.Parse(addressTxt.Text);
+                IPEndPoint endPoint = new IPEndPoint(addr, Convert.ToInt16(portTxt.Text));
+                Client.Start(endPoint);
+                
+                //request a token
+                Message requestToken = new Message();
+                requestToken.command = Command.Credentials;
+                requestToken.sender = User.GetIPAddress();
+                Properties.Settings.Default.ipv6 = requestToken.sender.ToString();
+                requestToken.content = userTxt.Text + "\n" + pwdTxt.Password;
+                Client.Send(requestToken);
+
+                //listen for token
+                Client.responseReceived += Client_responseReceived;
+                Client.Receive();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("The IP address of the server is not valid", "Data entered incorrectly", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            catch (InvalidCastException ex)
+            {
+                MessageBox.Show("The port must be a number", "Data entered incorrectly", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show("Error when trying to connect to the server. Check if the data is entered correctly", "Server unreachable", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Client_responseReceived(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.token = (Token) ((Message)e).content;
+
+            MainWindow main = new MainWindow();
+            Hide();
+            main.Show();
+            Close();
         }
     }
 }
