105bc7cd50ceae9a4f10efcd279b854b16d7b2a8
Luca <lucamozzo21173@gmail.com>
2016-07-10 17:48:47 +0200

    Handled case message > buffer

--- LANChat/LANChat Server/MessageReceiver.cs
+++ LANChat/LANChat Server/MessageReceiver.cs
@@ -9,45 +9,62 @@ namespace LANChat_Server
 {
     partial class Program
     {
+        private static List<User> users = new List<User>();
+
         private static void Server_MessageReceived(object sender, EventArgs e)
         {
             Message m = (Message)e;
-            switch (m.command)
+
+            try
             {
-                case Command.Credentials:
-                    //check credentials and start a session
-                    string username = ((string)m.content).Split('\n')[0];
-                    string password = ((string)m.content).Split('\n')[1];
-                    User user;
-                    try
-                    {
-                        DataRow row = Database.ExecuteQuery("SELECT Password, ID FROM Users WHERE Username='" + username + "';").Rows[0];
-                        if (((string)row.ItemArray[0]).Equals(password))
+                switch (m.command)
+                {
+                    case Command.Credentials:
+                        //check credentials and start a session
+                        string username = ((string)m.content).Split('\n')[0];
+                        string password = ((string)m.content).Split('\n')[1];
+                        User user;
+                        try
                         {
-                            user = new User(username, m.sender);
-                            string query = String.Format("INSERT INTO Session VALUES({0},'{1}','{2}');", row.ItemArray[1], user.token.signature, user.IP);
-                            Database.ExecuteNonQuery(query);
+                            DataRow row = Database.ExecuteQuery("SELECT Password, ID FROM Users WHERE Username='" + username + "';").Rows[0];
+                            if (((string)row.ItemArray[0]).Equals(password))
+                            {
+                                user = new User(username, m.sender);
+                                string query = String.Format("INSERT INTO Session VALUES({0},'{1}','{2}');", row.ItemArray[1], user.token.signature, user.IP);
+                                Database.ExecuteNonQuery(query);
 
-                            //send the token to the client
-                            Message response = new Message();
-                            response.content = user.token;
-                            Server.Send(response);
+                                //send the token to the client
+                                Message response = new Message();
+                                response.content = user.token;
+                                Server.Send(response);
+
+                                users.Add(user);
+                            }
                         }
-                    }
-                    catch(IndexOutOfRangeException ex)
-                    {
-                        //TODO no user found
-                    }
-                    break;
+                        catch (IndexOutOfRangeException ex)
+                        {
+                            //TODO no user found
+                        }
+                        break;
 
-                case Command.Login:
-                    break;
-                case Command.Logout:
-                    break;
-                case Command.Message:
-                    break;
-                case Command.Users:
-                    break;
+                    case Command.Login:
+                        break;
+                    case Command.Logout:
+                        break;
+                    case Command.Message:
+                        break;
+                    case Command.Users:
+                        //TODO check token first
+                        m = new Message();
+                        m.command = Command.Users;
+                        m.content = users;
+                        Server.Send(m);
+                        break;
+                }
+            }
+            catch(NullReferenceException ex)
+            {
+                //message > buffer
             }
         }
 
--- LANChat/Shared/Utils.cs
+++ LANChat/Shared/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
@@ -33,13 +34,23 @@ namespace Shared
 
         public static Object ByteArrayToObject(byte[] arrBytes)
         {
-            using (var memStream = new MemoryStream())
+            try
             {
-                var binForm = new BinaryFormatter();
-                memStream.Write(arrBytes, 0, arrBytes.Length);
-                memStream.Seek(0, SeekOrigin.Begin);
-                var obj = binForm.Deserialize(memStream);
-                return obj;
+                using (var memStream = new MemoryStream())
+                {
+                    var binForm = new BinaryFormatter();
+                    memStream.Write(arrBytes, 0, arrBytes.Length);
+                    memStream.Seek(0, SeekOrigin.Begin);
+
+
+                    var obj = binForm.Deserialize(memStream);
+                    return obj;
+                }
+            }
+            catch (SerializationException e)
+            {
+                WriteColour("The message you're trying to receive is bigger than the buffer", ConsoleColor.Red);
+                return null;
             }
         }
     }
